db7f2befc64c247e982f6da0f8b61204cb65e5b3
苗雨润 <miao.work2010@gmail.com>
2026-04-16 17:14:09 +0900

    添加插件描述信息

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   outDir: "dist",
   manifest: {
     name: "Page Clipper",
+    description: "小红书视频一键下载",
     icons: {
       16: "icons/icon16.png",
       48: "icons/icon48.png",
